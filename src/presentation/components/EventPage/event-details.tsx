18aee517e844e24f86b0fd45b8fa8e18714d4322
sontoong <vominhthang061003@gmail.com>
2025-04-14 17:25:00 +0700

    update read more

--- src/presentation/components/EventPage/event-details.tsx
+++ src/presentation/components/EventPage/event-details.tsx
@@ -1,8 +1,11 @@
 import { Card } from "antd";
-import React from "react";
+import React, { useState } from "react";
 import { Button } from "../common/button";
+import { clsx } from "clsx";
 
 export const EventDetails = () => {
+  const [readMore, setReadMore] = useState<boolean>(false);
+
   return (
     <Card
       styles={{ body: { padding: "12px 12px 0 12px" } }}
@@ -10,12 +13,31 @@ export const EventDetails = () => {
     >
       <div className="flex flex-col gap-3">
         <div className="text-base font-medium">Chi tiết hội thảo</div>
-        <ul className="list-disc pl-6 text-sm font-normal">
+        <ul
+          className={clsx("list-disc pl-6 text-sm font-normal", {
+            "line-clamp-[7]": !readMore,
+          })}
+        >
           <li>
             <span className="font-medium">14:00 - 14:30:</span> Cập nhật chẩn
             đoán, điều trị thoái hóa khớp gối, các quan điểm sai lầm và góc nhìn
             từ bác sĩ ngoại khoa: BS.CKII Phùng Cao Cường - khoa B1 BV 199
-          </li>{" "}
+          </li>
+          <li>
+            <span className="font-medium">14:30 - 15:00:</span> Phòng ngừa nguy
+            cơ tim phổi cho bệnh nhân COPD - chúng ta đang ở đâu?: BS.CKII
+            Nguyễn Thị Nhung - A4 BV 199
+          </li>
+          <li>
+            <span className="font-medium">14:30 - 15:00:</span> Phòng ngừa nguy
+            cơ tim phổi cho bệnh nhân COPD - chúng ta đang ở đâu?: BS.CKII
+            Nguyễn Thị Nhung - A4 BV 199
+          </li>
+          <li>
+            <span className="font-medium">14:30 - 15:00:</span> Phòng ngừa nguy
+            cơ tim phổi cho bệnh nhân COPD - chúng ta đang ở đâu?: BS.CKII
+            Nguyễn Thị Nhung - A4 BV 199
+          </li>
           <li>
             <span className="font-medium">14:30 - 15:00:</span> Phòng ngừa nguy
             cơ tim phổi cho bệnh nhân COPD - chúng ta đang ở đâu?: BS.CKII
@@ -28,8 +50,13 @@ export const EventDetails = () => {
         </div>
       </div>
       <Button
-        text={<div className="text-green5 text-sm font-semibold">Xem thêm</div>}
+        text={
+          <div className="text-green5 text-sm font-semibold">
+            {readMore ? "Ẩn bớt" : "Xem thêm"}
+          </div>
+        }
         className="w-full py-3"
+        onClick={() => setReadMore((prev) => !prev)}
       />
     </Card>
   );
